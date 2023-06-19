fef83071071fa7d3531d32d8def743738536ee28
FlutyJorge <flutyjorge@gmail.com>
2023-06-20 00:02:36 +0900

    Update PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//参考:https://nekojara.city/unity-input-system-character-controller
 public class PlayerController : MonoBehaviour
 {
+    [Header("パラメータ")]
+    [SerializeField] float walkSpeed;
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float gravity;
+    [SerializeField] float fallSpeed; //落下速度の制限
+    [SerializeField] float initFallSpeed; //落下の初速
+
+    private Transform transform;
+    private CharacterController charaCon;
+    private Vector2 input;
+    private float verticalVelocity;
+    private float turnVelocity;
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {

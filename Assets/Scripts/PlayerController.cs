588b6ed7e4e8c5a315dbdeea4eed45d0d93d5c00
FlutyJorge <flutyjorge@gmal.com>
2023-06-20 23:08:45 +0900

    update :Save PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-//参考:https://nekojara.city/unity-input-system-character-controller
+//参考:https://mochisakucoco.hatenablog.com/entry/2021/03/17/075601
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform stepRay;
+
+    [Space(10)]
     [Header("パラメータ")]
+    [SerializeField] bool isGrounded;
     [SerializeField] float walkSpeed;
     [SerializeField] float runSpeed;
-    [SerializeField] float jumpSpeed;
-    [SerializeField] float gravity;
-    [SerializeField] float fallSpeed; //落下速度の制限
-    [SerializeField] float initFallSpeed; //落下の初速
+    [SerializeField] float jumpPower;
+    [SerializeField] float stepDistance;
+    [SerializeField] float stepOffset;
+    [SerializeField] float slopeLimit;
+    [SerializeField] float slopeDistance;
 
-    private Transform transform;
-    private CharacterController charaCon;
-    private Vector2 input;
-    private float verticalVelocity;
-    private float turnVelocity;
-    private bool isGrounded;
+    private float horizontal;
+    private float vertical;
+    private Animator anim;
+    private Vector3 velocity;
+    private Vector3 input;
+    private Rigidbody rb;
+    private int layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        layerMask = ~(1 << LayerMask.NameToLayer("Player"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (isGrounded)
+        {
+            velocity = Vector3.zero;
+            input = new Vector3(horizontal, 0f, vertical);
+
+            //方向キーが推されている時
+            if (input.magnitude > 0)
+            {
+                Debug.DrawLine(transform.position + new Vector3(0, stepOffset, 0), transform.position + new Vector3(0, stepOffset, 0) + transform.forward * slopeDistance, Color.green);
+
+                
+            }
+        }
     }
 }
